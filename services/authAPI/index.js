57873eca7d0e60bf23d6157aae14cf5a354f1207
nuttapol roupipatpong <nuttapol.rou@gmail.com>
2018-05-19 15:51:08 +0700

    update package

--- services/authAPI/index.js
+++ services/authAPI/index.js
@@ -105,6 +105,9 @@ router.post("/verify", (req, res) => {
 router.post("/register", (req, res) => {
 
 	const data = req.body;
+	console.log(data.username);
+	console.log(data.password);
+	
 	if (data == undefined) {
 		res.json({ success: 0, message: "No data", });
 	} else if (data.username == undefined || data.username == "") {
