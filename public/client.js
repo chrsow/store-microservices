83e49bcf7a6d6cc1d0dba7cf49f20b19d63ecdd7
chrsow <liang_wasin@hotmail.co.th>
2018-05-19 17:16:50 +0700

    Update client login js file

--- public/client.js
+++ public/client.js
@@ -25,11 +25,36 @@
 	});
 
 	app.controller("HeaderController", function($http, $rootScope, $scope){
+		var client = this;
+		$rootScope.client = client;
+		client.isAuth = localStorage.getItem('user') || false;
 		$rootScope.cartCount = 0;
 		$http.get(cartApi + '/api/cart/count').success(function(res){
 			$rootScope.cartCount = res.count;
 		});
-  });
+
+		// Login & Register
+		this.username = '';
+		this.password = '';
+
+		this.login = function() {
+			$http.post(authApi + '/api/auth/login', {username: this.username, password: this.password}).success(function(res){
+				if(res.success){
+					// sessionStorage.user= angular.toJson({user: res.message});
+					localStorage.setItem('user',res.message);
+					client.isAuth = true;
+				}
+			});
+		}
+
+		this.register = function() {
+			$http.post(authApi + '/api/auth/register', {username: this.username, password: this.password}).success(function(res){
+				if(res.success){
+					
+				}
+			});
+		}
+	});
 
 	app.directive('storeHeader', function(){
 		return {
